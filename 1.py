f031335ddde42cc05b0fcd6126bcb94c613ba542
Viacheslav Bilous <viacheslav.bilous@gmail.com>
2017-12-15 12:50:27 +0200

    heelo gist
    tasks 1 and 5

--- 1.py
+++ 1.py
@@ -5,3 +5,18 @@ def hello(arg):
     '''
     return "Heelo, {}!".format(arg)
     
+def histogram(num_list):
+    '''
+    Procedure that takes a list of integers and prints a histogram to the screen.
+    >>> histogram([4, 9, 7])   
+    ****
+    *********
+    *******  
+    '''
+    import time
+    for num in num_list:
+        time.sleep(1.5)
+        print('*' * num)
+        
+        
+histogram([4, 9, 7])
